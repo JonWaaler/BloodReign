5bc6ea3b14858211b8fdd922ab7a937d56d2704c
Nathan <juthun@hotmail.com>
2019-02-15 00:43:34 -0500

    Fixed invis //and tweaked settings, # but particles are not invisiible

--- BloodReign/Assets/_Scripts/Abilities/InvisAbility.cs
+++ BloodReign/Assets/_Scripts/Abilities/InvisAbility.cs
@@ -25,45 +25,45 @@ public class InvisAbility : AbilityCommand
         {
             // set time for when next use of ability available
          //   nextAbil = Time.time + abilCool;
-            GameObject child = transform.GetChild(0).gameObject;
-            StartCoroutine(Invisible(lerpSpd, abilLength, child));
+            StartCoroutine(Invisible(lerpSpd, abilLength, transform.gameObject));
         }
     }
     private IEnumerator Invisible(float easeInOut, float duration, GameObject affectedObj) // ( timeToActivate, timeToLast, player's gun)
     {
         float current = 0.0f; // ElapsedTime
-        Renderer[] bodyParts = new Renderer[0];
-        if (affectedObj.transform.childCount > 0)
-        {
-            bodyParts = GetComponentsInChildren<Renderer>();
-        }
+        GameObject childBody = transform.GetChild(3).gameObject;
+
+        SkinnedMeshRenderer[] bodyParts = childBody.GetComponentsInChildren<SkinnedMeshRenderer>(true);
         // Ease in to invisiblity
         while (current <= easeInOut)
         {
             if (affectedObj.transform.childCount > 0)
             {
-                foreach (Renderer limb in bodyParts)
+                foreach (SkinnedMeshRenderer limb in bodyParts)
                 {
                     current = current + Time.deltaTime;
                     float percent = Mathf.Clamp01(current / easeInOut);
-                    Color alpha = affectedObj.GetComponent<Renderer>().material.color;
+                    Color alpha = limb.GetComponent<SkinnedMeshRenderer>().material.color;
                     alpha.a = 1 - percent;
                     limb.material.color = alpha;
+                    limb.GetComponent<SkinnedMeshRenderer>().enabled = false;
+
                 }
             }
             else
             {
                 current = current + Time.deltaTime;
                 float percent = Mathf.Clamp01(current / easeInOut);
-                Color alpha = affectedObj.GetComponent<Renderer>().material.color;
+                Color alpha = affectedObj.GetComponent<SkinnedMeshRenderer>().material.color;
                 alpha.a = 1 - percent;
-                affectedObj.GetComponent<Renderer>().material.color = alpha;
+                affectedObj.GetComponent<SkinnedMeshRenderer>().material.color = alpha;
+                affectedObj.GetComponent<SkinnedMeshRenderer>().enabled = false;
             }
             yield return null;
         }
         current = 0.0f;
         int activeGun = 0;
-        for (int i = 1; i < transform.childCount; i++)
+        for (int i = 1; i < transform.childCount-1; i++)
         {
             if (transform.GetChild(i).gameObject.activeSelf)
             {
@@ -71,7 +71,7 @@ public class InvisAbility : AbilityCommand
             }
         }
         GameObject child = transform.GetChild(activeGun).gameObject;
-        affectedObj.GetComponent<Renderer>().enabled = false;
+        //        affectedObj.GetComponent<SkinnedMeshRenderer>().enabled = false;
         child.GetComponent<Renderer>().enabled = false;
         // Duration for invisibility
         while (current <= duration)
@@ -80,29 +80,31 @@ public class InvisAbility : AbilityCommand
             yield return null;
         }
         // Ease back to Visible
-        affectedObj.GetComponent<Renderer>().enabled = true;
+//        affectedObj.GetComponent<SkinnedMeshRenderer>().enabled = true;
         child.GetComponent<Renderer>().enabled = true;
         current = 0.0f;
         while (current <= easeInOut)
         {
             if (affectedObj.transform.childCount > 0)
             {
-                foreach (Renderer limb in bodyParts)
+                foreach (SkinnedMeshRenderer limb in bodyParts)
                 {
                     current = current + Time.deltaTime;
                     float percent = Mathf.Clamp01(current / easeInOut);
-                    Color alpha = affectedObj.GetComponent<Renderer>().material.color;
+                    Color alpha = limb.GetComponent<SkinnedMeshRenderer>().material.color;
                     alpha.a = percent;
                     limb.material.color = alpha;
+                    limb.GetComponent<SkinnedMeshRenderer>().enabled = true;
                 }
             }
             else
             {
                 current = current + Time.deltaTime;
                 float percent = Mathf.Clamp01(current / easeInOut);
-                Color alpha = affectedObj.GetComponent<Renderer>().material.color;
+                Color alpha = affectedObj.GetComponent<SkinnedMeshRenderer>().material.color;
                 alpha.a = percent;
-                affectedObj.GetComponent<Renderer>().material.color = alpha;
+                affectedObj.GetComponent<SkinnedMeshRenderer>().material.color = alpha;
+                affectedObj.GetComponent<SkinnedMeshRenderer>().enabled = true;
             }
             yield return null;
         }
