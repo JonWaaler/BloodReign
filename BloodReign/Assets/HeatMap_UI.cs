7300313bd256009dbcc7170bf9a4230560ff5a4f
JonWaaler <jonwaaler@outlook.com>
2019-03-14 12:26:29 -0400

    +Blue plat fix

--- BloodReign/Assets/HeatMap_UI.cs
+++ BloodReign/Assets/HeatMap_UI.cs
@@ -2,16 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
 
 public class HeatMap_UI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    [Header("Heat Map Set-up")]
+    public GameObject heatSignature;
+    public Transform heatSignature_Parent;
+    public TextAsset deathData;
+
+    [Header("Random Amount (debug)")]
+    public int amtRandomPlacements = 1000;
+
+    private StreamReader reader;
+    private StreamWriter writer;
+
+	void Start ()
+    {
+        //FileInfo fileInfo = new FileInfo()
+
+
+        RandomPlacementOfHeatSignatures(amtRandomPlacements);
+        GenerateHeatMap();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void RandomPlacementOfHeatSignatures(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject sigInst = Instantiate(heatSignature);
+            sigInst.transform.SetParent(heatSignature_Parent);
+            sigInst.transform.position = new Vector3(Random.Range(-67.9f, 79.4f), 15, Random.Range(38f, -66.3f));
+        }
+    }
+
+    // Will run untill no lines left to read
+    // places heatSig at x and z location
+    void GenerateHeatMap()
+    {        
+        print(deathData.text);
+    }
 }
