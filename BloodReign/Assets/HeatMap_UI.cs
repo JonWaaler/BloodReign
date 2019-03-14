4301edbd714879f5776be7991a4b19640af6705a
JonWaaler <jonwaaler@outlook.com>
2019-03-14 13:42:02 -0400

    +Readline heatmap part done

--- BloodReign/Assets/HeatMap_UI.cs
+++ BloodReign/Assets/HeatMap_UI.cs
@@ -14,12 +14,12 @@ public class HeatMap_UI : MonoBehaviour {
     [Header("Random Amount (debug)")]
     public int amtRandomPlacements = 1000;
 
+    private FileInfo fileInfo;
     private StreamReader reader;
     private StreamWriter writer;
 
 	void Start ()
     {
-        //FileInfo fileInfo = new FileInfo()
 
 
         RandomPlacementOfHeatSignatures(amtRandomPlacements);
@@ -43,8 +43,51 @@ public class HeatMap_UI : MonoBehaviour {
 
     // Will run untill no lines left to read
     // places heatSig at x and z location
+    // READING PART
     void GenerateHeatMap()
-    {        
-        print(deathData.text);
+    {
+        // Text file in as file info
+        fileInfo = new FileInfo("E:/Source Tree/BloodReign/BloodReign/Assets/" + deathData.name + ".txt");
+        reader = fileInfo.OpenText();
+
+
+        string str_coords;
+
+        do
+        {
+            str_coords = reader.ReadLine();
+            if (str_coords != null)
+            {
+                // Get index of where the space is seperating x and z coords
+                int spaceIndex = str_coords.IndexOf(' ');
+                //print("Index: " + spaceIndex);
+
+                float x;
+                bool workedX = float.TryParse(str_coords.Substring(0, spaceIndex), out x);
+                float z;
+                bool workedZ = float.TryParse(str_coords.Substring(spaceIndex, str_coords.Length - spaceIndex), out z);
+
+                if (workedX && workedZ)
+                {
+                    //Debug
+                    //print("X:" + x + "        Z:" + z);
+
+                    GameObject sigInst = Instantiate(heatSignature);
+                    sigInst.transform.SetParent(heatSignature_Parent);
+                    sigInst.transform.position = new Vector3(x, 15, z);
+                }
+
+            }
+
+            //print(str_coords);
+        } while (str_coords != null);
+    }
+
+    // WRITING PART
+    void WriteSigCoords(float x, float z)
+    {
+        // Text file in as file info
+        fileInfo = new FileInfo("E:/Source Tree/BloodReign/BloodReign/Assets/" + deathData.name + ".txt");
+        //writer = fileInfo.OpenWrite();
     }
 }
