2ccf55fcc586f11caff49967d0ea8e39471dd558
Mitchell <mitchellfukuzawa@hotmail.com>
2019-03-15 09:35:38 -0400

    REVIEW 3 Build (might have death glitch)

--- BloodReign/Assets/_Scripts/Main Mechanics/Death.cs
+++ BloodReign/Assets/_Scripts/Main Mechanics/Death.cs
@@ -18,11 +18,12 @@ public class Death : MonoBehaviour {
         Debug.Log("Player Pos" + collision.transform.position);
         //behaviour.players.Remove(collision.transform);
     }
-    private void OnTriggerEnter(Collider other)
+
+    private void OnCollisionStay(Collision collision)
     {
         //collision.transform.position = Vector3.zero;
-        other.gameObject.GetComponent<WinDetection>().DamagePlayer(100);
-        Debug.Log("Player Pos" + other.transform.position);
+        collision.gameObject.GetComponent<WinDetection>().DamagePlayer(100);
+        Debug.Log("Player Pos" + collision.transform.position);
         //behaviour.players.Remove(collision.transform);
     }
 }
