122644a18f835edbabd7dafc8086e2f73ec8d274
JonWaaler <jonwaaler@outlook.com>
2019-03-15 08:32:35 -0400

    death thingy

--- BloodReign/Assets/_Scripts/Main Mechanics/Death.cs
+++ BloodReign/Assets/_Scripts/Main Mechanics/Death.cs
@@ -18,4 +18,11 @@ public class Death : MonoBehaviour {
         Debug.Log("Player Pos" + collision.transform.position);
         //behaviour.players.Remove(collision.transform);
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        //collision.transform.position = Vector3.zero;
+        other.gameObject.GetComponent<WinDetection>().DamagePlayer(100);
+        Debug.Log("Player Pos" + other.transform.position);
+        //behaviour.players.Remove(collision.transform);
+    }
 }
